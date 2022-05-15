04189d904c96c13980229758b00d3a12c8f4d7c4
JaumDarkz <viniciuspkfr@gmail.com>
2022-05-15 00:16:25 -0300

    feat: add main form and liq and transf navigation

--- src/components/Bridge.tsx
+++ src/components/Bridge.tsx
@@ -1,28 +1,45 @@
 import {useState} from 'react'
+import config from '../assets/config.png'
 
 function Bridge() {
 
-  function handleButtonChange() {
-    
+  const [select, setSelect] = useState('transfer')
+  const [activate, setActivate] = useState(true)
+
+  function handleButton(props:any) {
+    setSelect(props)
+    setActivate(true)
   }
   
   return (
     <div className="flex flex-col justify-start items-center">
-      <div className="h-12 bg-button-grey text-white rounded-lg p-1 my-7">
-        <button className='bg-button-blue text-white p-2 rounded-lg'>
+      <div className="flex h-16 px-2 bg-button-grey text-white rounded-3xl my-7 items-center">
+        <button className={select === 'transfer' ? 'bg-button-blue rounded-xl py-3 px-8 ease-linear duration-300' : 'text-white rounded-xl py-3 px-8 ease-linear duration-300'} onClick={() => handleButton('transfer')}>
           Transfer
         </button>
-        <button className='text-white text-lalezar p-2 rounded-lg'>
+        <button className={select === 'liquidity' ? 'bg-button-blue rounded-xl py-3 px-8 ease-linear duration-300' : 'text-white rounded-xl py-3 px-8 ease-linear duration-300'} onClick={() => handleButton('liquidity')}>
           Liquidity
         </button>
       </div>
-      <div className="text-secondary-grey bg-button-grey rounded-lg w-80 h-80">
-        <div className='flex flex-row-reverse p-3'>config</div>
-        <div className="">
-          <div className="flex flex-row p-2"><p className='pr-3'>from</p><form><input className='bg-background rounded-md p-1 w-75 h-10 -my-1' type="text" /></form></div>
-          <div className="flex flex-row p-2"><form><input  placeholder='send' className='bg-background rounded-md p-1 w-72 h-10' type="text" /></form></div>
+      
+      {select == 'transfer' ? 
+      
+      <div className="text-secondary-grey bg-button-grey rounded-lg pl-20 pr-20">
+        <div className='flex flex-row-reverse p-3'>
+          <img src={config} alt="Options" className='w-2/12' />
         </div>
+        <div className="">
+          <div className="flex flex-row p-2"><p className='pr-3'>from</p><form><input className='bg-background rounded-md p-1 -my-1' type="text" /></form></div>
+          <div className="flex flex-row p-2"><form><input  placeholder='send' className='bg-background rounded-md p-1' type="text" /></form></div>
+        </div>
+      </div> 
+      
+      : 
+      
+      <div className="text-secondary-grey bg-button-grey rounded-lg pl-20 pr-20">
+      cadeado asset
       </div>
+    }
     </div>
   )
 }
