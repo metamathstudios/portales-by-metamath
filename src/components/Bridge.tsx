da4772b32a3fda6150a9976a1145d8ee192cf054
JaumDarkz <viniciuspkfr@gmail.com>
2022-05-20 00:31:24 -0300

    feat: 1920x1080 and 1366x768 responsive footer finished

--- src/components/Bridge.tsx
+++ src/components/Bridge.tsx
@@ -36,7 +36,7 @@ function Bridge() {
   return (
     <>
     <div className="flex flex-col justify-start items-center">
-      <div className="flex h-12 px-2 bg-button-grey text-white rounded-2xl -my-16 mb-6 items-center bg-button-gray xl:mt-20">
+      <div className="flex h-12 px-2 bg-button-grey text-white rounded-2xl xl:-mt-16 mb-6 items-center bg-button-gray 2xl:mt-20">
         <button className={select === 'transfer' ? 'bg-button-blue rounded-xl py-2 px-8 ease-linear duration-300' : 'text-white rounded-xl py-3 px-8 ease-linear duration-300'} onClick={() => handleButton('transfer')}>
           Transfer
         </button>
@@ -50,7 +50,7 @@ function Bridge() {
       <div className="text-gray-200 bg-button-gray rounded-xl border-[1px] border-secondary-gray px-5 w-96">
         <div className="flex-wrap	w-[100%] pt-4">
           <div className="flex flex-row p-2 "><p className='pr-3 pt-2 text-xs'>From</p><button onClick={handleOpen} className='w-[40%] bg-background rounded-md py-2'><div className='flex justify-between px-2'><img className='' src={ethereum} width={25} alt='' /><img src={inputArrow} alt='' width={12} /></div></button></div>
-          <div className="flex flex-row p-2"><form className='w-[100%]'><input  placeholder={'Send: 0'} className='bg-background placeholder-gray-200 w-[100%] rounded-md p-2 py-3 text-xs' type="text" /><button onClick={handleOpen} className='absolute mt-2 lg:left-[56.6%] md:left-[55%] xl:left-[55.7%] bg-background rounded-md'><div className='flex justify-between space-x-2'><img className='' src={ethereum} width={25} alt='' /><img src={inputArrow} alt='' width={12} /></div></button></form></div>
+          <div className="flex flex-row p-2"><form className='w-[100%]'><input  placeholder={'Send: 0'} className='bg-background placeholder-gray-200 w-[100%] rounded-md p-2 py-3 text-xs' type="text" /><button onClick={handleOpen} className='absolute mt-2 xl:left-[58%] lg:left-[58%] 2xl:left-[55.7%] bg-background rounded-md'><div className='flex justify-between space-x-2'><img className='' src={ethereum} width={25} alt='' /><img src={inputArrow} alt='' width={12} /></div></button></form></div>
         </div>
         <div className='flex flex-row justify-between '>
           <div className='p-5'> </div> <button className='self-center rounded-lg my-3 py-3 px-1 border-transparent border-2 hover:border-2 hover:border-button-blue bg-background'><img src={arrows} alt="arrows" width={23} /></button>
